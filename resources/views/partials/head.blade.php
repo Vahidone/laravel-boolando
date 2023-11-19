31bf786ac9c8b4b7c2f5ceff0a510f6a549b2792
Vahidone <v.ban.roma@gmail.com>
2023-11-19 19:17:43 +0100

    feat: create head and include in main

---
+++ resources/views/partials/head.blade.php
@@ -0,0 +1,17 @@
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+       <!-- link FONT AWESOME  -->
+    <link
+    rel="stylesheet"
+    href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css"
+    integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA=="
+    crossorigin="anonymous"
+    />
+
+    <title>Laravel Booleando - @yield('title')</title>
+
+    <!-- Styles -->
+    @vite('resources/js/app.js')
+
+</head>
